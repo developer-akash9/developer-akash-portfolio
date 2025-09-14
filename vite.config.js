5f85b676213b2c6f547b0a055380186ced4dfc32
Your Name <akashjadhv2004@gmail.com>
2025-09-14 10:00:16 +0530

    Prepare for Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/developer-akash-portfolio/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000, // Further increase chunk size warning limit (in kB)
